6cd3c7726ab4403bdf060cc3bc4dc8fdab7d4382
Kitaetsss <kitayets1@mail.ru>
2022-02-01 21:44:03 +0500

    added hashCode and equals in Book/Author

--- src/ru/skypro/HW1/Book.java
+++ src/ru/skypro/HW1/Book.java
@@ -1,8 +1,12 @@
 package ru.skypro.HW1;
 
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.Objects;
+
 public class Book {
-    private String BookName;
-    private Author Author;
+    private final String BookName;
+    private final Author Author;
     private int yearBook;
 
     public Book(String BookName, int yearBook, Author Author){
@@ -21,5 +25,21 @@ public class Book {
     }
     public String getAuthor() {
         return Author.getAuthorName();
+    }
+    public String toString() {
+        return "Книга " + this.BookName + " год издания " + this.yearBook + " автор " + getAuthor();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearBook == book.yearBook && Objects.equals(BookName, book.BookName) && Objects.equals(Author, book.Author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(BookName, Author, yearBook);
     }
 }
--- src/ru/skypro/HW1/Main.java
+++ src/ru/skypro/HW1/Main.java
@@ -10,5 +10,14 @@ public class Main {
         System.out.println("Книга " + Pechal.getBookName() + ", год выпуска: " + Pechal.getYearBook() + ", автор книги " + Pechal.getAuthor());
         Radost.setYearBook(2016);
         System.out.println("Книга " + Radost.getBookName() + ", год выпуска: " + Radost.getYearBook() + ", автор книги " + Radost.getAuthor());
+        System.out.println(Pechal);
+        System.out.println(Radost);
+        Author author1 = new Author("Azamat");
+        Author author2 = new Author("Azamat");
+        System.out.println(author1.equals(author2));
+        System.out.println(author1 == author2);
+        Book book1 = new Book("Radost", 2016,author1);
+        Book book2 = new Book("Pechal",2017,author2);
+        System.out.println(book1.equals(book2));
     }
 }
